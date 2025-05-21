1f52bac69e7fed2da6676bb3a66c77f5e356e797
Jkl88 <35424901+Jkl88@users.noreply.github.com>
2025-05-21 16:30:39 +0300

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -11,7 +11,7 @@ async def register(data: RegisterRequest):
     return await register_user(data)
     
 @router.post("/login")
-async def login(data: LoginRequest = Depends()):
+async def login(data: LoginRequest):
     return await login_user(data)
     
 @router.post("/refresh")
